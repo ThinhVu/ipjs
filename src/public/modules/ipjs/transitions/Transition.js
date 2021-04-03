bfb6eb9170c014eec2234827d33ae6679ad2c096
ThinhVu <vutrongthinhk7@gmail.com>
2021-04-04 00:04:11 +0700

    Add gradient layer Add easing a lot of stuff

--- src/public/modules/ipjs/transitions/Transition.js
+++ src/public/modules/ipjs/transitions/Transition.js
@@ -1,19 +1,32 @@
 import {loadImages} from '../common/utils.js';
 
-export default async function trans(imgSrcs, config) {
-  const { duration, fps, transition, container } = config
+export default async function ({ imageSources, container, transition, stayDuration, loop }) {
   const containerEl = document.getElementById(container)
-  const images = await loadImages(imgSrcs)
+  const images = await loadImages(imageSources)
   let imageIndex = -1
   let nextImageIndex
+
   function run() {
     imageIndex++
     if (imageIndex === images.length - 1) {
-      return
+      if (loop) {
+        nextImageIndex = 0
+      } else {
+        return
+      }
+    } else if (imageIndex === images.length) {
+      imageIndex = 0;
+      nextImageIndex = imageIndex + 1
     } else {
       nextImageIndex = imageIndex + 1
     }
-    transition(images[imageIndex], images[nextImageIndex], containerEl, { duration, fps }).then(() => setTimeout(run, 1000))
+
+    transition.run(
+        images[imageIndex],
+        images[nextImageIndex],
+        containerEl
+    ).then(() => setTimeout(run, stayDuration))
   }
+
   run()
 }
--- src/public/modules/ipjs/transitions/modern-slide.js
+++ src/public/modules/ipjs/transitions/modern-slide.js
@@ -1,13 +1,13 @@
 import IPJs from '../index.js';
 import GaussianBlur from '../effects/gaussian-blur.js';
 import Pixmap from '../common/pixmap.js';
-import anims from './anims.js'
-import GrayScale from '../effects/gray-scale.js';
+import easing from './easing.js'
+import Additive from '../effects/additive.js';
 const ipJs = new IPJs()
-ipJs.add(new GaussianBlur(4))
-ipJs.add(new GrayScale())
+ipJs.add(new GaussianBlur(10))
+ipJs.add(new Additive([{ channel: 4, value: -20 }]))
 
-function makeBlurImage(img) {
+function makeImage(img, customImage) {
   return new Promise(resolve => {
     const w = img.width
     const h = img.height
@@ -15,18 +15,35 @@ function makeBlurImage(img) {
     c.width = w
     c.height = h
     const ctx = c.getContext("2d")
-    ctx.drawImage(img, 0 ,0)
-    const imgData = ctx.getImageData(0, 0, w, h)
-    ipJs.process(new Pixmap(imgData))
-    ctx.putImageData(imgData, 0, 0)
-    const blurImg = document.createElement("img")
-    blurImg.onload = () => resolve(blurImg)
-    blurImg.src = c.toDataURL("image/bmp")
+    customImage(ctx, w, h)
+    const outImg = document.createElement("img")
+    outImg.onload = () => resolve(outImg)
+    outImg.src = c.toDataURL("image/bmp")
   })
 }
 
-let baseCanvas, blurCanvas, canvas
-let baseCtx, blurCtx, ctx
+function makeGradientImg(img) {
+  return makeImage(img, (ctx, w, h) => {
+    const gradient = ctx.createLinearGradient(0,h, w, 0);
+    gradient.addColorStop(0, '#efff05');
+    gradient.addColorStop(1, '#a5fe98');
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, w, h);
+  })
+}
+
+function makeBlurImage(img) {
+  return makeImage(img, (ctx, w, h) => {
+    ctx.scale(2,2)
+    ctx.drawImage(img, -w/2 ,-h/2, w * 2, h * 2)
+    const imgData = ctx.getImageData(0, 0, w, h)
+    ipJs.process(new Pixmap(imgData))
+    ctx.putImageData(imgData, 0, 0)
+  })
+}
+
+let baseCanvas, gradientCanvas, blurCanvas, canvas
+let baseCtx, gradientCtx, blurCtx, ctx
 
 function initCanvas(container, zIndex) {
   const c = document.createElement('canvas')
@@ -40,58 +57,89 @@ function initCanvasIfNeeded(container, w, h) {
   if (baseCanvas == null)
     [baseCanvas, baseCtx] = initCanvas(container, w, h, 0)
 
+  if (gradientCanvas == null)
+    [gradientCanvas, gradientCtx] = initCanvas(container, w, h, 1)
+
   if (blurCanvas == null)
-    [blurCanvas, blurCtx] = initCanvas(container, w, h, 1)
+    [blurCanvas, blurCtx] = initCanvas(container, w, h, 2)
 
   if (canvas == null)
-    [canvas, ctx] = initCanvas(container, w, h, 2)
+    [canvas, ctx] = initCanvas(container, w, h, 3)
 }
 
 // https://motionarray.com/browse/after-effects-templates/transitions
-export default async function transition(preImg, nextImg, container, { duration, fps }) {
-  const w = container.clientWidth
-  const h = container.clientHeight
-  initCanvasIfNeeded(container)
-
-  const blurImg = await makeBlurImage(nextImg);
-
-  // Draw the ellipse
-  const origin = { x: 0, y: 0 }
-  function makePath(radius) {
-    const path = new Path2D()
-    path.ellipse(origin.x, origin.y, radius, radius, 0, 0, 2 * Math.PI);
-    path.lineTo(origin.x, origin.y)
-    path.closePath()
-    return path
+export default class ModernSlide {
+  constructor({ transitionDuration, fps, origin, easing }) {
+    this.transitionDuration =  transitionDuration
+    this.fps = fps
+    this.origin = origin || { x: 0, y: 0 }
+    this.easing = easing || 'easeInSine'
   }
 
-  const msPerStep = 1000 / fps;
-  const maxRadius = Math.sqrt(w * w + h * h)
-  const totalStep = duration / msPerStep;
-  let currentStep = 0
-  let radius = 0;
-  let distance = 0;
+  async run(preImg, nextImg, container) {
+    const _t = this
+    const w = container.clientWidth
+    const h = container.clientHeight
+    initCanvasIfNeeded(container)
 
-  return new Promise(resolve => {
-    function anim() {
-      currentStep++
-      ctx.save()
-      blurCtx.save()
-      radius = Math.round(maxRadius * anims.easeInOutQuart(currentStep/totalStep))
-      distance = 100 * Math.round(Math.sin(currentStep/totalStep * Math.PI))
-      blurCtx.clip(makePath(radius + distance), "evenodd")
-      blurCtx.drawImage(blurImg, 0, 0)
-      ctx.clip(makePath(radius), "evenodd")
-      ctx.drawImage(nextImg, 0, 0)
-      ctx.restore()
-      blurCtx.restore()
-      if (currentStep < totalStep) {
-        setTimeout(anim, msPerStep)
-      } else {
-        return resolve()
-      }
+    const gradientImg = await makeGradientImg(nextImg);
+    const blurImg = await makeBlurImage(nextImg);
+
+    // Draw the ellipse
+    function makePath(radius) {
+      const path = new Path2D()
+      path.ellipse(_t.origin.x, _t.origin.y, radius, radius, 0, 0, 2 * Math.PI);
+      path.lineTo(_t.origin.x, _t.origin.y)
+      path.closePath()
+      return path
     }
-    baseCtx.drawImage(preImg, 0, 0)
-    anim()
-  })
+
+    const msPerStep = 1000 / _t.fps;
+    const maxRadius = Math.sqrt(w * w + h * h)
+    const totalStep = _t.transitionDuration / msPerStep;
+    let currentStep = 0
+
+    //
+    let gradientRadius = 0;
+    let blurRadius = 0;
+    let radius = 0;
+
+    return new Promise(resolve => {
+      function anim() {
+        currentStep++
+        ctx.save()
+        blurCtx.save()
+        gradientCtx.save()
+
+        gradientRadius = Math.round(maxRadius * easing[_t.easing](Math.min(currentStep, totalStep)/totalStep))
+        gradientCtx.clip(makePath(gradientRadius), "nonzero")
+        gradientCtx.drawImage(gradientImg, 0, 0)
+
+        blurRadius = Math.round(maxRadius * easing[_t.easing](Math.max(currentStep - 10, 0)/totalStep))
+        blurCtx.clip(makePath(blurRadius), "evenodd")
+        blurCtx.drawImage(blurImg, 0, 0)
+
+        radius = Math.round(maxRadius * easing[_t.easing](Math.max(currentStep - 20, 0)/totalStep))
+        ctx.clip(makePath(radius), "evenodd")
+        ctx.drawImage(nextImg, 0, 0)
+
+        // remove clip
+        ctx.restore()
+        blurCtx.restore()
+        gradientCtx.restore()
+
+        if (currentStep < totalStep) {
+          setTimeout(anim, msPerStep)
+        } else {
+          return resolve()
+        }
+      }
+
+      baseCtx.drawImage(preImg, 0, 0)
+      gradientCtx.clearRect(0,0,w,h)
+      blurCtx.clearRect(0,0,w,h);
+      ctx.clearRect(0,0,w,h);
+      anim()
+    })
+  }
 }
